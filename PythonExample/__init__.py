1d05df11c42f41c78bdcfbd49a081a3281190737
Fetics <ril50066@gmail.com>
2015-12-28 10:19:09 +0800

    NewPythonCode
    NewPythonCode

---
+++ PythonExample/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import random;
+
+mylist = [0 , "hello world" , 123 , "測試"];
+
+if  "測試" in mylist:
+    print("Yes");
+else:
+    print("No");
---
+++ PythonExample/basic/List.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+"""
+
+"""
+from testModule import print_lol
+
+mylist = [0 , "hello world" , 123 , "����"];
+
+if  "����" in mylist:
+    print("Yes");
+else:
+    print("No");
+    
+    
+mylist = [0 , "hello world" , 123 , "TEST" , ["ele1" , "ele2" , "ele4"]];
+
+print(mylist)
+print_lol(mylist)
